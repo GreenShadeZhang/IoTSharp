a9900080bedef1f96907edf157066d5e6717675e
gil zhang <gil.zhang.dev@outlook.com>
2023-08-01 23:35:30 +0800

    添加回调接口

---
+++ IoTSharp/Controllers/QinglanCallbackController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using IoTSharp.Contracts;
+using IoTSharp.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace IoTSharp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class QinglanCallbackController : ControllerBase
+    {
+        private readonly ILogger<QinglanCallbackController> _logger;
+        public QinglanCallbackController(ILogger<QinglanCallbackController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public  Task<ApiResult> PersonCount([FromBody] QinglanPersonCountDto dto)
+        {
+            _logger.LogInformation($"call back start at time：{DateTime.Now.ToString()}");
+            _logger.LogInformation(System.Text.Json.JsonSerializer.Serialize(dto));
+            return Task.FromResult(new ApiResult()
+            {
+                Code = 200,
+                Msg = ""
+            });
+        }
+    }
+}
---
+++ IoTSharp/Dtos/QinglanPersonCountDto.cs
@@ -0,0 +1,25 @@
+﻿namespace IoTSharp.Dtos
+{
+    public class QinglanPersonCountDto
+    {
+        /// <summary>
+        /// 标识
+        /// </summary>
+        public string Cmd { get; set; }
+
+        /// <summary>
+        /// 设备ID
+        /// </summary>
+        public string Uid { get; set; }
+
+        /// <summary>
+        /// 人数变更
+        /// </summary>
+        public string Event { get; set; }
+
+        /// <summary>
+        /// 变更人数
+        /// </summary>
+        public int Count { get; set; }
+    }
+}
